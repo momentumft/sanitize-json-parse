63fbd68aa4c46a88dba93494dda597c6f02014b5
Dave Tonge <dave.tonge@momentumft.co.uk>
2017-01-04 17:38:19 +0000

    Added support for wrapping classes

--- index.js
+++ index.js
@@ -6,32 +6,41 @@ const patch = () => {
   const old = JSON.parse.bind(JSON)
   JSON.parse = function() {
     const result = old.apply(null, arguments)
-    return traverse(result).forEach((node) => {
+    traverse(result).forEach((node) => {
       if(node && typeof node === "object") {
         map.set(node, 1)
       }
     })
+    return result
   }
 }
 
-patch()
-
-
-const patchConsumer = (fn) => {
+const patchFn = (fn, context) => {
   return function(...args) {
     traverse(args).forEach((node) => {
       if(node && typeof node === "object" && map.get(node)){
         throw new Error("Unsanitized user input passed to sanitized function")
       }
     })
-    return fn.apply(null, arguments)
+    return fn.apply(context, arguments)
   }
 }
 
-module.exports = (fn) => {
+const patchObj = (obj) => {
+  return traverse(obj).map(function(node) {
+    if(node && typeof node === "function") {
+      this.update(patchFn(node, this.parent))
+    }
+  })
+}
+
+module.exports = (thing) => {
   if(!map) {
     map = new WeakMap
     patch()
   }
-  return patchConsumer(fn)
+  if(typeof thing === "function") {
+    return patchFn(fn)
+  }
+  return patchObj(thing)
 }
